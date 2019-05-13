da4cb5990492f3da2d4be6c34fab33c4645ce7e9
diego7perez <49918844+diego7perez@users.noreply.github.com>
2019-05-13 17:22:43 +0200

    Update bin_cnt_presc.vhd

--- bin_cnt_presc.vhd
+++ bin_cnt_presc.vhd
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------
 -- Brno University of Technology, Department of Radio Electronics
 --------------------------------------------------------------------------------
--- Author: HALA AMAHJOUR, DIEGO PEREZ
+-- Author: DIEGO PEREZ
 -- Date: 2019-04-18
 -- Design: bin_cnt_presc
 -- Description: N-bit binary counter with synchronous reset whose functionality 
